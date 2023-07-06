64a3a9a24ca08e395fe026462c14b7ba4c36cd94
wonjonghong <wonhong1996@naver.com>
2023-07-06 12:56:25 +0900

    2023-07-06

---
+++ food_cost_calculator_15.0/lib/big one/sales_report_detail.dart
@@ -0,0 +1,220 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:food_cost_calculator_3_0/small one/custom_appbar.dart';
+import 'package:food_cost_calculator_3_0/logic/pie_chart_logic.dart';
+import 'package:food_cost_calculator_3_0/small one/pie_chart.dart';
+import 'package:food_cost_calculator_3_0/logic/chart_colors.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:food_cost_calculator_3_0/small one/cost_bar_graph.dart';
+
+class SalesReportDetailPage extends StatefulWidget {
+  final String reportId;
+
+  const SalesReportDetailPage({required this.reportId, Key? key}) : super(key: key);
+
+  @override
+  _SalesReportDetailPageState createState() => _SalesReportDetailPageState();
+}
+
+class _SalesReportDetailPageState extends State<SalesReportDetailPage> {
+  late Future<DocumentSnapshot> futureReport;
+
+  @override
+  void initState() {
+    super.initState();
+    final user = FirebaseAuth.instance.currentUser;
+    futureReport = FirebaseFirestore.instance
+        .collection('users')
+        .doc(user?.uid)
+        .collection('SalesReports')
+        .doc(widget.reportId)
+        .get();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final lang = AppLocalizations.of(context)!;
+    final formatCurrency = NumberFormat('#,##0', 'en_US');
+
+    return Scaffold(
+      appBar: const MyAppBar(title: "보고서 상세"),
+      body: FutureBuilder<DocumentSnapshot>(
+        future: futureReport,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return const Center(child: Text('오류가 발생했습니다.'));
+          }
+
+          if (snapshot.hasData && snapshot.data != null) {
+            final data = snapshot.data!.data() as Map<String, dynamic>;
+
+            final name = data['name'] as String? ?? '제목 없음';
+            final date = (data['date'] as Timestamp).toDate();
+            final period = data['period'] as String? ?? '기간 정보 없음';
+            final totalRevenue = data['data']['totalRevenue'] as double? ?? 0.0;
+            final totalCost = (data['data']['totalCost'] as num? ?? 0.0).toInt();
+            final preTaxProfit = totalRevenue - totalCost;
+            final preTaxProfitFormatted = formatCurrency.format(preTaxProfit);
+            final totalRevenueFormatted = formatCurrency.format(totalRevenue);
+            final costListByFoodType = data['data']['costListByFoodType'] as Map<String, dynamic>? ?? {};
+            final totalRevenueByFoodType = data['data']['totalRevenueByFoodType'] as Map<String, dynamic>? ?? {};
+            final profitByFoodType = data['data']['profitByFoodType'] as Map<String, dynamic>? ?? {};
+            final costRateByFoodType = data['data']['costRateByFoodType'] as Map<String, dynamic>? ?? {};
+
+            final costItems = (data['data']['costListByFoodType'] as Map<String, dynamic>? ?? {}).entries.expand((entry) {
+              return (entry.value as List<dynamic>).map((costItem) => MapEntry<String, double>(
+                costItem['name'] as String,
+                (costItem['unitCost'] as num).toDouble() * (costItem['isFixedCostPerUnit'] ?? false ? 1.0 : (costItem['quantity'] as num).toDouble() ?? 1.0),
+              ));
+            }).toList();
+
+
+            costItems.sort((a, b) => b.value.compareTo(a.value));
+            final top10CostItems = costItems.take(10).map((entry) => MapEntry(entry.key, entry.value.toDouble())).toList();
+
+            return ListView(
+              padding: const EdgeInsets.all(16.0),
+              children: <Widget>[
+                Card(
+                  shape: const RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                  ),
+                  child: ListTile(
+                    title: Text('이름', style: Theme.of(context).textTheme.titleMedium),
+                    subtitle: Text(name, style: Theme.of(context).textTheme.headlineSmall),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Card(
+                  shape: const RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('작성일자', style: Theme.of(context).textTheme.titleMedium),
+                            Text(DateFormat('yyyy-MM-dd').format(date), style: Theme.of(context).textTheme.titleSmall),
+                          ],
+                        ),
+                        const Spacer(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text('대상 기간', style: Theme.of(context).textTheme.titleMedium),
+                            Text("$period 월", style: Theme.of(context).textTheme.titleSmall),
+                          ],
+                        ),
+                        const Spacer(flex: 2),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                Card(
+                  shape: const RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                  ),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text('총 매출액', style: Theme.of(context).textTheme.titleMedium),
+                        subtitle: Text("$totalRevenueFormatted ${lang.calculationPage_name_of_currency}", style: Theme.of(context).textTheme.titleSmall),
+                      ),
+                      ListTile(
+                        title: Text('세전 순이익', style: Theme.of(context).textTheme.titleMedium),
+                        subtitle: Text("$preTaxProfitFormatted ${lang.calculationPage_name_of_currency}", style: Theme.of(context).textTheme.titleSmall),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,  // Aligns the children evenly in the row
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CustomPieChart(
+                          title: "매출액 기여도",
+                          sections: generateData(totalRevenueByFoodType, colors),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CustomPieChart(
+                          title: "순이익 기여도",
+                          sections: generateData(profitByFoodType, colors),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CostBarChart(data: top10CostItems),
+                  ),
+                ),
+                for (final entry in costListByFoodType.entries)
+                  Card(
+                    shape: const RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                    ),
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(entry.key, style: Theme.of(context).textTheme.titleLarge ),
+                          const SizedBox(height: 8.0),
+                          if ((entry.value as List<dynamic>).isNotEmpty)
+                            Text('총 판매량: ${(entry.value as List<dynamic>)[0]['quantity'] ?? '정보 없음'} ${lang.calculationPage_name_of_unit}, 음식 가격: ${formatCurrency.format((entry.value as List<dynamic>)[0]['foodPrice'] as double? ?? 0.0)} ${lang.calculationPage_name_of_currency}', style: Theme.of(context).textTheme.bodyMedium),
+                            Text('원가율: ${((costRateByFoodType[entry.key] as double? ?? 0.0)).toStringAsFixed(1)}%', style: Theme.of(context).textTheme.bodyMedium),
+
+                          const SizedBox(height: 8.0),
+                          ExpansionTile(
+                            title: Text('세부 원가 보기:', style: Theme.of(context).textTheme.titleSmall),
+                            children: [
+                              for (var costItemMap in (entry.value as List<dynamic>))
+                                ListTile(
+                                  title: Text(
+                                    '항목명: ${costItemMap['name']}',
+                                    style: TextStyle(color: (costItemMap['isFixedCostPerUnit'] ?? false) ? Colors.blue : Colors.red),
+                                  ),
+                                  subtitle: Text(
+                                    '${(costItemMap['isFixedCostPerUnit'] ?? false) ? '원가 금액(고정)' : '원가 금액'}: ${formatCurrency.format(costItemMap['unitCost'])} ${lang.calculationPage_name_of_currency}',
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+              ],
+            );
+          } else {
+            return const Center(child: Text('데이터가 없습니다.'));
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+
+
---
+++ food_cost_calculator_15.0/lib/small one/single_ai_report_button.dart
@@ -0,0 +1,154 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+
+class SingleReportAnalysisButton extends StatelessWidget {
+  final ValueNotifier<List<String>> checkedList;
+  final User? user;
+  final FirebaseFirestore _firestore;
+
+  SingleReportAnalysisButton({Key? key, required this.checkedList, required this.user, required FirebaseFirestore firestore}) : _firestore = firestore, super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: 250,
+        height: 60,
+        child: ElevatedButton(
+          onPressed: () async {
+            if (checkedList.value.length != 1) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('한 개의 보고서만 선택해주세요. 현재 보고서는 개별적으로만 분석 가능합니다.'),
+                  duration: Duration(milliseconds: 1000),
+                  behavior: SnackBarBehavior.floating,
+                ),
+              );
+            } else {
+              final reports = _firestore.collection('users').doc(user?.uid).collection('SalesReports');
+              final reportDoc = await reports.doc(checkedList.value[0]).get();
+
+              // Convert the report data into a single string
+              Map<String, dynamic>? reportData = reportDoc.data();
+              String prompt = "";
+              String reportName = "";
+              reportData?.forEach((key, value) {
+                if (key == "name") {
+                  reportName = value;
+                }
+                prompt += "$key: $value\n";
+              });
+
+              final TextEditingController questionController = TextEditingController();
+
+              // ignore: use_build_context_synchronously
+              bool? shouldSend = await showDialog<bool>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return WillPopScope(
+                      onWillPop: () async => false, // Prevent dialog from closing on outside click
+                      child: AlertDialog(
+                        title: const Text('gpt에게 질문하고 싶은 부분은?'),
+                        content: Container(
+                          height: 150.0,
+                          child: TextField(
+                            controller: questionController,
+                            decoration: const InputDecoration(
+                              hintText: "여기에 질문을 입력하세요",
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 1.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                              ),
+                            ),
+                            maxLines: null,
+                            keyboardType: TextInputType.multiline,
+                          ),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: const Text('취소'),
+                            style: TextButton.styleFrom(
+                              primary: Colors.deepPurpleAccent,
+                              backgroundColor: Colors.white,
+                              side: const BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop(false);
+                            },
+                          ),
+                          TextButton(
+                            child: const Text('전송'),
+                            style: TextButton.styleFrom(
+                              primary: Colors.deepPurpleAccent,
+                              backgroundColor: Colors.white,
+                              side: const BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop(true);
+                            },
+                          ),
+                        ],
+                      )
+                    );
+                  }
+              );
+
+              if (shouldSend == null || !shouldSend) {
+                // If shouldSend is null or false, do not proceed with the report submission
+                return;
+              }
+
+              String userQuestion = questionController.text;
+              if (userQuestion.isNotEmpty) {
+                prompt += "\n\n$userQuestion";
+              }
+
+              prompt += "\n\n 니가 자영업자에게 매출분석을 해주는 회계사라고 생각해. 앞의 내용을 바탕으로 현재 가게 운영 상황을 분석해서 한국말로 문단마다 띄어쓰기 해줘. 지금까지 기본 설정이었고, 이 다음 내용은 실제 사용자의 질문내용이니까 그 질문에 우선적으로 답해줘.";
+
+              final gptReplies = _firestore.collection('users').doc(user?.uid).collection('gpt_Replies');
+              await gptReplies.add({
+                'prompt': prompt,
+                'question': userQuestion, // Add this line to store the user's question
+                'reportName': reportName,
+                'parentMessageId': '${user?.uid}_${DateTime.now().millisecondsSinceEpoch}'
+              });
+              // ignore: use_build_context_synchronously
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('서버에 보고서를 전달했습니다. 잠시 후에 AI분석 내역에서 확인하실 수 있습니다.'),
+                  duration: Duration(milliseconds: 5000),
+                  behavior: SnackBarBehavior.floating,
+                ),
+              );
+            }
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.white),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: 24.0,
+              ),
+            ),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+                side: const BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+              ),
+            ),
+            overlayColor: MaterialStateProperty.all(Colors.deepPurpleAccent.withOpacity(0.1)),
+          ),
+          child: const Text(
+            'AI에게 분석 요청(개별 보고서)',
+            style: TextStyle(fontSize: 20.0, color: Colors.deepPurpleAccent),
+          ),
+        ),
+      ),
+    );
+  }
+}
