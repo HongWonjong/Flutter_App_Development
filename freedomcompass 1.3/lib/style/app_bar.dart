bcb30f13f69359ee6a05be4a6df6c5fe1cef3976
wonjonghong <wonhong1996@naver.com>
2024-01-03 00:35:55 +0900

    2024-01-03

--- freedomcompass 1.3/lib/style/app_bar.dart
+++ freedomcompass 1.3/lib/style/app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freedomcompass/style/text_style.dart';
+import 'package:freedomcompass/style/color.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String? titleText;
@@ -16,12 +17,11 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       centerTitle: true,
-      title: FittedBox(fit: BoxFit.scaleDown,
-     child: Text(
+      title: Text(
       titleText ?? "글자 안넣었을 때 나오는 글자",
       style: AdaptiveText.titleTextStyle(context, color: Colors.black),
-    ), ),
-      backgroundColor: Colors.blueGrey,
+    ),
+      backgroundColor: AppColors.appBarColor,
     );
   }
 }
