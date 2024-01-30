b4445f0f689e8b66252a36fbbb15fb55c53e097c
wonjonghong <wonhong1996@naver.com>
2024-01-30 21:20:32 +0900

    2023-1-30

--- 심심해서 만든 웹사이트 1.0/lib/function/delete_memo.dart
+++
@@ -1,9 +0,0 @@
-// delete_functions.dart
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-Future<void> deleteMemo(String memoId) async {
-  String uid = FirebaseAuth.instance.currentUser?.uid ?? "";
-  await FirebaseFirestore.instance.collection('user').doc(uid).collection('memoes').doc(memoId).delete();
-}
