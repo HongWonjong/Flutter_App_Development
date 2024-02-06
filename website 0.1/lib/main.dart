40b99b8b707555835d76625ef5a4df38fe445d49
wonjonghong <wonhong1996@naver.com>
2024-02-06 22:02:12 +0900

    2024-02-06

--- website 0.1/lib/main.dart
+++ website 0.1/lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'webpage.dart'; // 메인 파일의 이름에 따라 수정
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Firebase App Check 초기화
+  await FirebaseAppCheck.instance.activate(
+    webProvider: ReCaptchaV3Provider("6LcJG2kpAAAAAJZMe2b-oth304J4WqmDRFA2dJhp"),
   );
 
   runApp(MyApp());
