19a8cfe72122f7059cacb86e3f05cc117932505b
wonjonghong <wonhong1996@naver.com>
2024-02-08 23:25:55 +0900

    2024-02-08

---
+++ website 0.3/lib/style/image_path.dart
@@ -0,0 +1,25 @@
+// image_paths.dart
+class ImagePaths {
+  static final List<String> imagePath1 = [
+    'lib/images/그지.jpg',
+    'lib/images/그지.jpg',
+    'lib/images/그지.jpg',
+    'lib/images/그지.jpg',
+    // Add more image paths as needed
+  ];
+
+  static final List<String> imagePath2 = [
+    'lib/images/한잔해.jpg',
+    'lib/images/한잔해.jpg',
+    'lib/images/한잔해.jpg',
+    'lib/images/한잔해.jpg',
+    // Add more image paths as needed
+  ];
+  static final List<String> imagePath3 = [
+    'lib/images/뻐끔뻐끔.gif',
+    'lib/images/뻐끔뻐끔.gif',
+    'lib/images/뻐끔뻐끔.gif',
+    'lib/images/뻐끔뻐끔.gif',
+    // Add more image paths as needed
+  ];
+}
