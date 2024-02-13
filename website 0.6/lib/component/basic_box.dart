dedbff8dd04d77f8d367a95ac051dea9e304cf8f
wonjonghong <wonhong1996@naver.com>
2024-02-13 15:48:59 +0900

    2024-02-13

---
+++ website 0.6/lib/component/basic_box.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:website/style/color.dart';
+import 'package:website/style/language.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:website/function/send_prompt.dart';
+
+class BasicBox extends StatefulWidget {
+  final double height;
+  final double width;
+  final Color backgroundColor;
+  final double borderRadius;
+  final Color borderColor;
+  final double borderWidth;
+  final EdgeInsets margin;
+  final EdgeInsets padding;
+  final Widget? child;
+  final String title;
+
+
+  const BasicBox({
+    Key? key,
+    required this.height,
+    required this.width,
+    this.backgroundColor = AppColors.bodyPageBackground,
+    this.borderRadius = 10.0,
+    this.borderColor = Colors.grey,
+    this.borderWidth = 1.0,
+    this.margin = const EdgeInsets.all(8.0),
+    this.padding = const EdgeInsets.all(16.0),
+    this.child,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  _BasicBoxState createState() => _BasicBoxState();
+}
+
+class _BasicBoxState extends State<BasicBox> {
+  final TextEditingController _textController = TextEditingController();
+  bool isTextEmpty = true;
+  String selectedModel = MainPageLan.modelNameGemini;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController.addListener(() {
+      setState(() {
+        isTextEmpty = _textController.text.isEmpty;
+      });
+    });
+  }
+
+  void sendPromptToModel() {
+    if (!isTextEmpty) {
+      String uid = FirebaseAuth.instance.currentUser!.uid;
+      if (selectedModel == MainPageLan.modelNameGemini) {
+        sendGeminiPromptToFirestore(uid, _textController.text);
+      } else if (selectedModel == MainPageLan.modelNameGpt35) {
+        sendGPT35PromptToFirestore(uid, _textController.text);
+      }
+      _textController.clear();
+    }
+  }
+
+  void onDropdownChanged(String? newValue) {
+    setState(() {
+      selectedModel = newValue!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+        borderRadius: BorderRadius.circular(widget.borderRadius),
+        border: Border.all(
+          color: widget.borderColor,
+          width: widget.borderWidth,
+        ),
+      ),
+      margin: widget.margin,
+      padding: widget.padding,
+      height: widget.height,
+      width: widget.width,
+      child: Column(
+        children: [
+          Text(
+            widget.title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: widget.child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
---
+++ website 0.6/lib/webpage.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'style/media_query_custom.dart';
+import 'component/custom_app_bar.dart';
+import 'component/question_and_answer_box.dart';
+import 'style/language.dart';
+import 'component/header.dart';
+import 'style/image_path.dart';
+import 'function/google_auth.dart';
+import 'style/color.dart';
+import 'component/message_response_list.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'component/chat_log_box.dart';
+import 'function/get_response.dart';
+import 'component/basic_box.dart';
+
+
+class MyApp extends ConsumerWidget {
+   MyApp({Key? key}) : super(key: key);
+  AuthFunctions authFunctions = AuthFunctions();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:  const CustomAppBar(),
+        body: Stack(
+          children: [
+            /* Gradient background */
+            Container(
+              decoration:   BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    AppColors.bodyGradationLeft,
+                    AppColors.bodyGradationRight,
+                  ],
+                ),
+              ),
+            ),
+
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: MQSize.getDetailHeight1(context)),
+                  SizedBox(height: MQSize.getDetailHeight1(context)),
+                  HeaderPage(
+                    height: MQSize.getDetailHeight4(context),
+                    width: MQSize.getDetailWidth99(context),
+                    imagePaths: ImagePaths.imagePath2,
+                    imageHeight: MQSize.getDetailHeight4(context),
+                    imageWidth: MQSize.getDetailWidth4(context),
+                    textYouWant: MainPageLan.briefExplanation,
+                  ),
+                  SizedBox(height: MQSize.getDetailHeight1(context)),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: QABox(
+                          height: MQSize.getDetailHeightHalf(context),
+                          width: MQSize.getDetailWidth90(context),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      EmptyBox(
+                        title: MainPageLan.geminiPro,
+                        height: MQSize.getDetailHeight70(context),
+                        width: MQSize.getDetailWidthHalf(context),
+                        deleteDocArg: FunctionLan.geminiDoc,
+                        child: MessageListWidget(modelResponseStream: listenForGeminiProResponse()),
+                      ),
+                      Expanded(
+                        child: EmptyBox(
+                          title: MainPageLan.gpt35,
+                          height: MQSize.getDetailHeight70(context),
+                          width: MQSize.getDetailWidthHalf(context),
+                          deleteDocArg: FunctionLan.gpt35Doc,
+                          child: MessageListWidget(modelResponseStream: listenForGPT35Response()),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: BasicBox(
+                          title: MainPageLan.autoGpt,
+                          height: MQSize.getDetailHeightHalf(context),
+                          width: MQSize.getDetailWidth99(context),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
