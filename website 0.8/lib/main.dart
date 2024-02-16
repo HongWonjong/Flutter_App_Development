ca79412d18c6920439a93ec711a31bdb75afff3b
wonjonghong <wonhong1996@naver.com>
2024-02-16 14:10:33 +0900

    2024-02-15

--- website 0.8/lib/main.dart
+++ website 0.8/lib/main.dart
@@ -9,9 +9,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 void main() async {
-  await dotenv.load(fileName: ".env");
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+
+  WidgetsFlutterBinding.ensureInitialized(); // 웹사이트 개발에 필수적인 플러터엔진과 위젯을 바인딩함
+  await dotenv.load(fileName: ".env"); // 환경변수 불러오기
+
+  await Firebase.initializeApp( // 파이어베이스 초기화
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
@@ -20,9 +22,8 @@ void main() async {
     webProvider: ReCaptchaV3Provider(dotenv.env['RECAPTCHA_TOKEN']!),
   );
 
-
   runApp(
-    ProviderScope(
+    ProviderScope( // 앱 전체에서 리버팟을 사용할 수 있도록!
       child: MyApp(),
     ),
   );
