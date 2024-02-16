78d4c9137dce6f7da60982327c33496106e6176a
wonjonghong <wonhong1996@naver.com>
2024-02-16 17:15:50 +0900

    2024-02-16

--- website 0.8/lib/main.dart
+++ website 0.8/lib/main.dart
@@ -9,26 +9,26 @@ import 'package:cloud_functions/cloud_functions.dart';
 
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  WidgetsFlutterBinding.ensureInitialized(); // 웹사이트 개발에 필수적인 플러터엔진과 위젯을 바인딩함
-
-  await Firebase.initializeApp( // 파이어베이스 초기화
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  Future<String> fetchSecretValue() async { //구글 시크릿 매니저의 리캡쳐 토큰값을 가져올 것이다.
-    HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('getSecretValue');
-    final response = await callable.call();
-    return response.data['secretValue'];
+    String secretValue = await fetchSecretValue();
+    await FirebaseAppCheck.instance.activate(
+      webProvider: ReCaptchaV3Provider(secretValue),
+    );
+  } catch (e) {
+    print("Firebase initialization error: $e");
   }
-  // 비동기 함수를 사용하여 ReCaptchaV3Provider를 초기화하기
-  String secretValue = await fetchSecretValue();
-  await FirebaseAppCheck.instance.activate(
-    webProvider: ReCaptchaV3Provider(secretValue),
-  );
 
-  runApp(
-    ProviderScope( // 앱 전체에서 리버팟을 사용할 수 있도록!
-      child: MyApp(),
-    ),
-  );
+  runApp(ProviderScope(child: MyApp()));
+}
+
+// 이 함수는 main 함수 외부에 위치해야 합니다.
+Future<String> fetchSecretValue() async {
+  HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('getSecretValue');
+  final response = await callable.call();
+  return response.data['secretValue'];
 }
