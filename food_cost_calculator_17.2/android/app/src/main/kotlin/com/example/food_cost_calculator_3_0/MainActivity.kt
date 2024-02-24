83c31990bbe56acc8d36a444f2df908bc7374c7d
wonjonghong <wonhong1996@naver.com>
2024-02-24 21:22:08 +0900

    2024-02-24 - 로그인 로직 개멍청하게 만든거 누구임? 그건 작년의 나..

--- food_cost_calculator_17.2/android/app/src/main/kotlin/com/example/food_cost_calculator_3_0/MainActivity.kt
+++ food_cost_calculator_17.2/android/app/src/main/kotlin/com/example/food_cost_calculator_3_0/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.example.food_cost_calculator_3_0
 
-class MainActivity: android.app.Activity() {
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
 }
