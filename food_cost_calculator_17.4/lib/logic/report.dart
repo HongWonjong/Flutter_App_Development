266640d67b2aa122f4c8d7b2e919fb6c6774a8ef
wonjonghong <wonhong1996@naver.com>
2024-03-01 14:11:19 +0900

    2024-02-24

---
+++ food_cost_calculator_17.4/lib/logic/report.dart
@@ -0,0 +1,9 @@
+class Report {
+  final String id;
+  final double totalSales;
+  final String period;
+  final double netProfit;
+  final double totalCost;
+
+  Report(this.id, this.totalSales, this.period, this.netProfit, this.totalCost);
+}
