12255787354c114b726a3abec923cc02d947c847
wonjonghong <wonhong1996@naver.com>
2025-03-17 13:41:49 +0900

    2025_03_17: 콘솔 게임을 만드는 중. RpgGame 클래스 내의 메서드들의 상호작용을 통한 콘솔 내 이동 로직 구현과 플레이어 아이템과 체력, 마나, 공격력, 방어력 등 계수, 상인이 판매하는 아이템과 그 속성, 설명 등을 추가하고 구매 로직을 추가함.

---
+++ dart_console_rpg_game/bin/dart_console_rpg_game.dart
@@ -0,0 +1,8 @@
+import 'classes/rpg_game.dart';
+
+
+void main(List<String> arguments) {
+  RpgGame rpgGame = RpgGame();
+  print("나만의 콘솔 어드벤쳐에 오신 것을 환영합니다! 갈수록 어려워지는 던전을 답파하고 \n 돈을 모아 마을에서 더 강해지세요!");
+  rpgGame.run();
+}
