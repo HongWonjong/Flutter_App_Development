ba009d3b78ed767ab98b19ea86b72228a6803fa1
wonjonghong <wonhong1996@naver.com>
2025-03-18 20:10:25 +0900

    2025_03_18 update: 이해를 돕기 위한 주석 대폭  추가

--- dart_console_rpg_game/bin/functions/monster_skill.dart
+++ dart_console_rpg_game/bin/functions/monster_skill.dart
@@ -6,13 +6,13 @@ import 'dart:math';
 void berserk(RpgGame game, Monster monster) {
   print("${monster.name}가 광폭화했습니다! 공격력이 ${monster.atk + 20}으로 증가하고 방어력이 ${monster.def + 10} 증가합니다.");
   monster.skill_used = true;
-}
+} // 몬스터의 공/방 처럼 final로 선언된 것들은 인스턴스 자체의 값을 바꿀 수 없으므로 원래 값을 별도의 변수로 복사하여 사용한다.
 
 // hardenedClaws 는 웨어울프의 특수 전용기로, 플레이어의 방어력의 절반 만큼을 자신의 이번 공격에 합산하여 공격한다. 맞으면 아프다.
 void hardenedClaws(RpgGame game, Monster monster) {
   int extraDamage = (game.player.totalDef ~/ 2);
   int baseDamage = monster.atk + extraDamage;
-  int totalDamage = (baseDamage - game.player.totalDef).clamp(0, baseDamage);
+  int totalDamage = (baseDamage - game.player.totalDef).clamp(0, baseDamage); //방어력 계산 시, 음수가 되거나 하지 않게 제한을 둔다.
   print("${monster.name}가 발톱을 순간적으로 경화해서 방어구를 꿰뚫는 공격을 날립니다, 총 피해: $totalDamage");
   game.player.hpNow -= totalDamage;
 }
@@ -21,7 +21,7 @@ void hardenedClaws(RpgGame game, Monster monster) {
 void regenerate(RpgGame game, Monster monster) {
   int healAmount = 20;
   print("${monster.name}의 상처가 빠르게 재생됩니다! HP가 $healAmount 회복되었습니다.");
-  // HP 회복은 dungeon 메서드에서 직접 처리해야 함 (monster.hp는 final이므로)
+  // 마찬가지로 HP 회복은 dungeon 메서드에서 직접 처리해야 한다(monster.hp는 final이므로)
 }
 
 // 마법사 유령은 공격력의 1.5배 데미지의 파이어볼을 날릴 수 있다.
@@ -74,7 +74,8 @@ void tentaclePush(RpgGame game, BossMonster boss) async {
   int totalDamage = 0;
   for (int i = 0; i < hits; i++) {
     int baseDamage = (boss.atk * (1 + i * 0.2)).toInt(); // 다단히트 강화
-    int damage = (baseDamage - (game.player.totalDef ~/ 2)).clamp(0, baseDamage); // Player의 totalDef 사용
+    // 매 다단히트 계산 시 Player의 totalDef이 반영은 되지만, 절반만 반영되므로 아프게 맞는다.
+    int damage = (baseDamage - (game.player.totalDef ~/ 2)).clamp(0, baseDamage);
     totalDamage += damage;
     game.player.hpNow -= damage; // Player의 hpNow 감소
     print("${boss.name}의 촉수가 갑옷을 꿰뚫는 날카로운 창으로 변해 당신을 밀칩니다! $damage 데미지!");
